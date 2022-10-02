6dfe00428cd61505d17405d1634705434ebbe4bc
Michael Morandell <michael.morandell@tfobz.net>
2022-10-02 15:58:46 +0200

    new MediaPortal

--- MediaPortal/src/app/app.module.ts
+++ MediaPortal/src/app/app.module.ts
@@ -1,45 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { VideoDBService } from './shared/video-db.service';
+
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { NewtestcompComponent } from './newtestcomp/newtestcomp.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { WebsiteComponent } from './website/website.component';
-import { FormComponent } from './form/form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ReactiveformComponent } from './reactiveform/reactiveform.component';
-
-const myRoutes:Routes = [
-  { path: 'new/:id', component: NewtestcompComponent },
-  { path: 'form', component: FormComponent },
-  { path: 'reactiveform', component: ReactiveformComponent },
-  { path: 'web', component: WebsiteComponent },
-  { path: '', component: TitleComponent, pathMatch: "full"},
-  { path: '**', redirectTo: "new/404"},
-];
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TitleComponent,
-    NewtestcompComponent,
-    WebsiteComponent,
-    FormComponent,
-    ReactiveformComponent
+    AppComponent
   ],
   imports: [
-    RouterModule.forRoot(myRoutes),
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [VideoDBService, {provide: "VideoComponentConfig", useValue: "meineConfig"}],
+  providers: [],
   bootstrap: [AppComponent]
 })
-//kann durch export von anderen modules verwendet werden
 export class AppModule { }
