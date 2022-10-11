e632f34685599958716ffe0fe3904584f3efc766
Michael Morandell <michael.morandell@tfobz.net>
2022-10-11 20:28:04 +0200

    added alternative home

--- MediaPortal/src/app/app.module.ts
+++ MediaPortal/src/app/app.module.ts
@@ -10,9 +10,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { LoginComponent } from './login/login.component';
+import { MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { AltHomeComponent } from './alt-home/alt-home.component';
 
 const routes:Routes = [
-  { path: '', component: HomepageComponent},
+  { path: '', component: HomepageComponent },
+  { path: 'alt', component: AltHomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: '404', component: NotFoundPageComponent },
   { path: '**', redirectTo: '404', pathMatch: 'full' }
 ]
@@ -23,6 +30,8 @@ const routes:Routes = [
     AppComponent,
     HomepageComponent,
     NotFoundPageComponent,
+    LoginComponent,
+    AltHomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +40,9 @@ const routes:Routes = [
     MatIconModule,
     MatButtonModule,
     MatDividerModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
