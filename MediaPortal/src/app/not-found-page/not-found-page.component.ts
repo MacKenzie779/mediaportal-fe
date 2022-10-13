6f5ab362e723f0167440308c3579704bc301f10b
Michael Morandell <michael.morandell@tfobz.net>
2022-10-13 22:41:16 +0200

    new 404 page v1 solves issu #1

--- MediaPortal/src/app/not-found-page/not-found-page.component.ts
+++ MediaPortal/src/app/not-found-page/not-found-page.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundPageComponent implements OnInit {
 
+  mySidebar:any = null;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  open_sidebar(): void {
+    this.mySidebar = document.getElementById("mySidebar");
+    if (this.mySidebar.style.display === 'block') {
+      this.mySidebar.style.display = 'none';
+    } else {
+      this.mySidebar.style.display = 'block';
+    }
+  }
+
+  close_sidebar(): void {
+    this.mySidebar.style.display = "none";
+  }
+
 
 }
