793d4349d8d68535b707ccf12268f6746ebacee3
Michael Morandell <michael.morandell@tfobz.net>
2022-10-29 17:12:29 +0200

    added download ui

--- MediaPortal/src/app/youtube/youtube.component.ts
+++ MediaPortal/src/app/youtube/youtube.component.ts
@@ -1,5 +1,13 @@
+import { Media } from './../_models/media';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+//import environment var
+import { environment } from '@environments/environment';
+import { AuthenticationService } from '@app/_services';
+import { first } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-youtube',
@@ -10,16 +18,21 @@ export class YoutubeComponent implements OnInit {
 
   error:any = "";
   loginForm!: FormGroup;
-
+  downloadForm!: FormGroup;
+  currentMedia:Media;
   mySidebar:any = null;
 
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor(private formBuilder: FormBuilder, private http : HttpClient, private authenticationService: AuthenticationService) {
+    this.currentMedia = new Media();
+  }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       url: ['', Validators.required],
       convType: ['', Validators.required]
     });
+    this.downloadForm = this.formBuilder.group({});
   }
 
   open_sidebar(): void {
@@ -35,8 +48,28 @@ export class YoutubeComponent implements OnInit {
     this.mySidebar.style.display = "none";
   }
 
+  get field () {
+    return this.loginForm.controls;
+  }
+
   onSubmit() {
-    console.log("submit");
+    var uri = this.loginForm.controls['url'].value;
+    var format:Number = Number(this.loginForm.controls['convType'].value);
+    var uid = this.authenticationService.currentUserValue['id'];
+    this.http.post<any>(`${environment.apiUrl}/data/yt_dl`, {uid, uri, format}).subscribe({
+      next: (resp) => {
+        this.currentMedia = new Media();
+        this.currentMedia.filepath = resp['filename'];
+        this.currentMedia.mid = resp['mid'];
+      },
+      error: error => {
+        this.error = error;
+      }
+    });
+  }
+
+  onDownload() {
+    console.log("download converted file");
   }
 
 }
