7c44a2e93042d734535d6124bf351dc45c536946
Michael Morandell <michael.morandell@tfobz.net>
2022-10-31 11:40:27 +0100

    changed nav bar for better experience solves #12

--- MediaPortal/src/app/changepasswd/changepasswd.component.ts
+++ MediaPortal/src/app/changepasswd/changepasswd.component.ts
@@ -21,7 +21,7 @@ export class ChangepasswdComponent implements OnInit {
   loginForm!: FormGroup;
   loading = false;
   submitted = false;
-  //errors on login, shown to the user
+  //errors on login
   error:any = '';
   isError:boolean = true;
   user:User;
